70a33d6264698d1fe506c1eeb25b488cf527f66b
Dongseon <elwynn94@gmail.com>
2024-06-04 23:36:28 +0900

    Feat : entity - CommentTimeStamped.java 추가

--- src/main/java/com/sparta/springnewsfeed/entity/Comment.java
+++ src/main/java/com/sparta/springnewsfeed/entity/Comment.java
@@ -1,11 +1,23 @@
 package com.sparta.springnewsfeed.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data // // Lombok 어노테이션으로 getter, setter, toString, equals, hashCode를 자동 생성
+@NoArgsConstructor
 public class Comment {
-    private long username; // id
-    private long post_id; // 게시글 id
-    private long author_id; // 작성자 id
-    private String content; // 내용물
-    private long comment_like; // 좋아요 갯수
-    private long comment_created_at; // 댓글 생성일자
-    private long comment_updated_at; // 댓글 수정일자
+
+    @Id
+    private Long commentId; // 댓글 ID
+
+    private long postId; // 게시글 ID
+    private long username; // 작성자 이름
+    private String commentContent; // 내용물
+    private long commentLike; // 좋아요 갯수
 }
