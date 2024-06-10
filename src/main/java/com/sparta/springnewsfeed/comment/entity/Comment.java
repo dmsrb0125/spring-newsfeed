2f9fe9bd6fc62e4d12bac1854de2d1aa253d1b0c
Dongseon <elwynn94@gmail.com>
2024-06-10 16:08:26 +0900

    Update Comment.java

--- src/main/java/com/sparta/springnewsfeed/comment/entity/Comment.java
+++ src/main/java/com/sparta/springnewsfeed/comment/entity/Comment.java
@@ -6,14 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.userdetails.User;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
